107f96828a6b6558e67faa374d8b662006995e28
Vaishnavi Mantha <2018vmantha@donkeykong.csl.tjhsst.edu>
2017-11-15 13:28:46 -0500

    movement track

---
+++ movement_find.py
@@ -0,0 +1,25 @@
+
+# detects background vs. foreground based on motion
+
+import cv2
+
+import numpy as np
+
+cap = cv2.VideoCapture('ball.mp4')
+#fgbg = cv2.createBackgroundSubtractorMOG2()   # only works on mac
+fgbg = cv2.BackgroundSubtractorMOG()   # works on linux
+
+while True:
+    ret, frame = cap.read()
+    fgmask = fgbg.apply(frame)
+
+    cv2.imshow('original', frame)
+    cv2.imshow('frame', fgmask)
+
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
